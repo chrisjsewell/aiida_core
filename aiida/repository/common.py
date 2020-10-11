flavianojs/aiida-core

Add the base implementation for the new repository
The `Repository` class is the new interface to interact with the file
repository. It contains a file hierarchy in memory to now what is
contained within it. A new instance can be constructed from an existing
hierarchy using the `from_serialized` class method.

The actual storing of the objects in a repository is delegated to a
backend repository instance, represented by `AbstractRepositoryBackend`.
This backend class is only tasked with consuming byte streams and
storing them as objects in some repository under a unique key that
allows the object to be retrieved at a later time. The backend
repository is not expected to keep a mapping of what it contains. It
should merely provide the interface to store byte streams and return
their content, given their unique corresponding key.

This also means that the internal virtual hierarchy of a ``Repository``
instance does not necessarily represent all the files that are stored by
repository backend. The repository exposes a mere subset of all the file
objects stored in the backend. This is why object deletion is also
implemented as a soft delete, by default, where the files are just
removed from the internal virtual hierarchy, but not in the actual
backend. This is because those objects can be referenced by other
instances.

The interface is implemented for the `disk-objectstore` which is an
efficient key-value store on the local file system. This commit also
provides a sandbox implementation which represents a temporary folder on
the local file system.

aiida/repository/common.py
@@ -1,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""Module with resources common to the repository."""
+import enum
+import typing
+
+__all__ = ('FileType', 'FileObject')
+
+
+class FileType(enum.Enum):
+    """Enumeration to represent the type of a file object."""
+
+    DIRECTORY = 0
+    FILE = 1
+
+
+class FileObject():
+    """Data class representing a file object."""
+
+    def __init__(
+        self,
+        name: str = '',
+        file_type: FileType = FileType.DIRECTORY,
+        key: typing.Union[str, None] = None,
+        objects: typing.Dict[str, 'FileObject'] = None
+    ):
+        if not isinstance(name, str):
+            raise TypeError('name should be a string.')
+
+        if not isinstance(file_type, FileType):
+            raise TypeError('file_type should be an instance of `FileType`.')
+
+        if key is not None and not isinstance(key, str):
+            raise TypeError('key should be `None` or a string.')
+
+        if objects is not None and any([not isinstance(obj, self.__class__) for obj in objects.values()]):
+            raise TypeError('objects should be `None` or a dictionary of `FileObject` instances.')
+
+        self._name = name
+        self._file_type = file_type
+        self._key = key
+        self._objects = objects or {}
+
+    @classmethod
+    def from_serialized(cls, serialized: dict) -> 'FileObject':
+        """Construct a new instance from a serialized instance.
+
+        :param serialized: the serialized instance.
+        :return: the reconstructed file object.
+        """
+        objects = {name: FileObject.from_serialized(obj) for name, obj in serialized['objects'].items()}
+        instance = cls.__new__(cls)
+        instance.__init__(serialized['name'], FileType(serialized['file_type']), serialized['key'], objects)
+        return instance
+
+    def serialize(self) -> dict:
+        """Serialize the metadata into a JSON-serializable format.
+
+        :return: dictionary with the content metadata.
+        """
+        return {
+            'name': self.name,
+            'file_type': self.file_type.value,
+            'key': self.key,
+            'objects': {key: obj.serialize() for key, obj in self.objects.items()},
+        }
+
+    @property
+    def name(self) -> str:
+        """Return the name of the file object."""
+        return self._name
+
+    @property
+    def file_type(self) -> FileType:
+        """Return the file type of the file object."""
+        return self._file_type
+
+    @property
+    def key(self) -> typing.Union[str, None]:
+        """Return the key of the file object."""
+        return self._key
+
+    @property
+    def objects(self) -> typing.Dict[str, 'FileObject']:
+        """Return the objects of the file object."""
+        return self._objects
+
+    def __eq__(self, other) -> bool:
+        """Return whether this instance is equal to another file object instance."""
+        if not isinstance(other, self.__class__):
+            return False
+
+        equal_attributes = all([getattr(self, key) == getattr(other, key) for key in ['name', 'file_type', 'key']])
+        equal_object_keys = list(self.objects) == list(other.objects)
+        equal_objects = equal_object_keys and all([obj == other.objects[key] for key, obj in self.objects.items()])
+
+        return equal_attributes and equal_objects
+
+    def __repr__(self):
+        args = (self.name, self.file_type.value, self.key, list(self.objects.keys()))
+        return 'FileObject<name={}, file_type={}, key={}, objects={}>'.format(*args)

tests/repository/test_common.py
@@ -1,0 +1,109 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=redefined-outer-name
+"""Tests for the :mod:`aiida.repository.common` module."""
+import pytest
+
+from aiida.repository import FileObject, FileType
+
+
+@pytest.fixture
+def file_object() -> FileObject:
+    """Test fixture to create and return a ``FileObject`` instance."""
+    name = 'relative'
+    file_type = FileType.FILE
+    key = 'abcdef'
+    objects = {'sub': FileObject()}
+    return FileObject(name, file_type, key, objects)
+
+
+def test_constructor():
+    """Test the constructor defaults."""
+    file_object = FileObject()
+    assert file_object.name == ''
+    assert file_object.file_type == FileType.DIRECTORY
+    assert file_object.key is None
+    assert file_object.objects == {}
+
+
+def test_constructor_kwargs(file_object: FileObject):
+    """Test the constructor specifying specific keyword arguments."""
+    name = 'relative'
+    file_type = FileType.FILE
+    key = 'abcdef'
+    objects = {'sub': FileObject()}
+    file_object = FileObject(name, file_type, key, objects)
+
+    assert file_object.name == 'relative'
+    assert file_object.file_type == FileType.FILE
+    assert file_object.key == 'abcdef'
+    assert file_object.objects == objects
+
+
+def test_constructor_kwargs_invalid():
+    """Test the constructor specifying invalid keyword arguments."""
+    name = 'relative'
+    file_type = FileType.FILE
+    key = 'abcdef'
+    objects = {'sub': FileObject()}
+
+    with pytest.raises(TypeError):
+        FileObject(None, file_type, key, objects)
+
+    with pytest.raises(TypeError):
+        FileObject(name, None, key, objects)
+
+    with pytest.raises(TypeError):
+        FileObject(name, file_type, 123, objects)
+
+    with pytest.raises(TypeError):
+        FileObject(name, file_type, key, {'sub': FileObject, 'wrong': 'type'})
+
+
+def test_serialize(file_object: FileObject):
+    """Test the ``FileObject.serialize`` method."""
+    expected = {
+        'name': file_object.name,
+        'file_type': file_object.file_type.value,
+        'key': file_object.key,
+        'objects': {
+            'sub': {
+                'name': '',
+                'file_type': FileType.DIRECTORY.value,
+                'key': None,
+                'objects': {},
+            }
+        }
+    }
+
+    assert file_object.serialize() == expected
+
+
+def test_serialize_roundtrip(file_object: FileObject):
+    """Test the serialization round trip."""
+    serialized = file_object.serialize()
+    reconstructed = FileObject.from_serialized(serialized)
+
+    assert isinstance(reconstructed, FileObject)
+    assert file_object == reconstructed
+
+
+def test_eq():
+    """Test the ``FileObject.__eq__`` method."""
+    file_object = FileObject()
+
+    # Identity operation
+    assert file_object == file_object  # pylint: disable=comparison-with-itself
+
+    # Identical default copy
+    assert file_object == FileObject()
+
+    # Identical copy with different arguments
+    assert FileObject(name='custom', file_type=FileType.FILE) == FileObject(name='custom', file_type=FileType.FILE)
+
+    # Identical copies with nested objects
+    assert FileObject(objects={'sub': FileObject()}) == FileObject(objects={'sub': FileObject()})
+
+    assert file_object != FileObject(name='custom')
+    assert file_object != FileObject(file_type=FileType.FILE)
+    assert file_object != FileObject(key='123456')
+    assert file_object != FileObject(objects={'sub': FileObject()})

chrisjsewell/aiida_core

aiida/repository/common.py
@@ -7,14 +7,11 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
-# pylint: disable=redefined-builtin
 """Module with resources common to the repository."""
 import enum
-import warnings
+import typing
 
-from aiida.common.warnings import AiidaDeprecationWarning
-
-__all__ = ('File', 'FileType')
+__all__ = ('FileType', 'File')
 
 
 class FileType(enum.Enum):
@@ -24,30 +21,56 @@ class FileType(enum.Enum):
     FILE = 1
 
 
-class File:
+class File():
     """Data class representing a file object."""
 
-    def __init__(self, name: str = '', file_type: FileType = FileType.DIRECTORY, type=None):
-        """
-
-            .. deprecated:: 1.4.0
-                The argument `type` has been deprecated and will be removed in `v2.0.0`, use `file_type` instead.
-        """
-        if type is not None:
-            warnings.warn(
-                'argument `type` is deprecated and will be removed in `v2.0.0`. Use `file_type` instead.',
-                AiidaDeprecationWarning
-            )  # pylint: disable=no-member"""
-            file_type = type
-
+    def __init__(
+        self,
+        name: str = '',
+        file_type: FileType = FileType.DIRECTORY,
+        key: typing.Union[str, None] = None,
+        objects: typing.Dict[str, 'File'] = None
+    ):
         if not isinstance(name, str):
             raise TypeError('name should be a string.')
 
         if not isinstance(file_type, FileType):
             raise TypeError('file_type should be an instance of `FileType`.')
 
+        if key is not None and not isinstance(key, str):
+            raise TypeError('key should be `None` or a string.')
+
+        if objects is not None and any([not isinstance(obj, self.__class__) for obj in objects.values()]):
+            raise TypeError('objects should be `None` or a dictionary of `File` instances.')
+
         self._name = name
         self._file_type = file_type
+        self._key = key
+        self._objects = objects or {}
+
+    @classmethod
+    def from_serialized(cls, serialized: dict) -> 'File':
+        """Construct a new instance from a serialized instance.
+
+        :param serialized: the serialized instance.
+        :return: the reconstructed file object.
+        """
+        objects = {name: File.from_serialized(obj) for name, obj in serialized['objects'].items()}
+        instance = cls.__new__(cls)
+        instance.__init__(serialized['name'], FileType(serialized['file_type']), serialized['key'], objects)
+        return instance
+
+    def serialize(self) -> dict:
+        """Serialize the metadata into a JSON-serializable format.
+
+        :return: dictionary with the content metadata.
+        """
+        return {
+            'name': self.name,
+            'file_type': self.file_type.value,
+            'key': self.key,
+            'objects': {key: obj.serialize() for key, obj in self.objects.items()},
+        }
 
     @property
     def name(self) -> str:
@@ -55,28 +78,31 @@ class File:
         return self._name
 
     @property
-    def type(self) -> FileType:
-        """Return the file type of the file object.
-
-        .. deprecated:: 1.4.0
-            Will be removed in `v2.0.0`, use `file_type` instead.
-        """
-        warnings.warn('property is deprecated, use `file_type` instead', AiidaDeprecationWarning)  # pylint: disable=no-member"""
-        return self.file_type
-
-    @property
     def file_type(self) -> FileType:
         """Return the file type of the file object."""
         return self._file_type
 
-    def __iter__(self):
-        """Iterate over the properties."""
-        warnings.warn(
-            '`File` has changed from named tuple into class and from `v2.0.0` will no longer be iterable',
-            AiidaDeprecationWarning
-        )
-        yield self.name
-        yield self.file_type
+    @property
+    def key(self) -> typing.Union[str, None]:
+        """Return the key of the file object."""
+        return self._key
 
-    def __eq__(self, other):
-        return self.file_type == other.file_type and self.name == other.name
+    @property
+    def objects(self) -> typing.Dict[str, 'File']:
+        """Return the objects of the file object."""
+        return self._objects
+
+    def __eq__(self, other) -> bool:
+        """Return whether this instance is equal to another file object instance."""
+        if not isinstance(other, self.__class__):
+            return False
+
+        equal_attributes = all([getattr(self, key) == getattr(other, key) for key in ['name', 'file_type', 'key']])
+        equal_object_keys = list(self.objects) == list(other.objects)
+        equal_objects = equal_object_keys and all([obj == other.objects[key] for key, obj in self.objects.items()])
+
+        return equal_attributes and equal_objects
+
+    def __repr__(self):
+        args = (self.name, self.file_type.value, self.key, list(self.objects.keys()))
+        return 'File<name={}, file_type={}, key={}, objects={}>'.format(*args)

tests/repository/test_common.py
@@ -1,0 +1,109 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=redefined-outer-name
+"""Tests for the :mod:`aiida.repository.common` module."""
+import pytest
+
+from aiida.repository import File, FileType
+
+
+@pytest.fixture
+def file_object() -> File:
+    """Test fixture to create and return a ``File`` instance."""
+    name = 'relative'
+    file_type = FileType.FILE
+    key = 'abcdef'
+    objects = {'sub': File()}
+    return File(name, file_type, key, objects)
+
+
+def test_constructor():
+    """Test the constructor defaults."""
+    file_object = File()
+    assert file_object.name == ''
+    assert file_object.file_type == FileType.DIRECTORY
+    assert file_object.key is None
+    assert file_object.objects == {}
+
+
+def test_constructor_kwargs(file_object: File):
+    """Test the constructor specifying specific keyword arguments."""
+    name = 'relative'
+    file_type = FileType.FILE
+    key = 'abcdef'
+    objects = {'sub': File()}
+    file_object = File(name, file_type, key, objects)
+
+    assert file_object.name == 'relative'
+    assert file_object.file_type == FileType.FILE
+    assert file_object.key == 'abcdef'
+    assert file_object.objects == objects
+
+
+def test_constructor_kwargs_invalid():
+    """Test the constructor specifying invalid keyword arguments."""
+    name = 'relative'
+    file_type = FileType.FILE
+    key = 'abcdef'
+    objects = {'sub': File()}
+
+    with pytest.raises(TypeError):
+        File(None, file_type, key, objects)
+
+    with pytest.raises(TypeError):
+        File(name, None, key, objects)
+
+    with pytest.raises(TypeError):
+        File(name, file_type, 123, objects)
+
+    with pytest.raises(TypeError):
+        File(name, file_type, key, {'sub': File, 'wrong': 'type'})
+
+
+def test_serialize(file_object: File):
+    """Test the ``File.serialize`` method."""
+    expected = {
+        'name': file_object.name,
+        'file_type': file_object.file_type.value,
+        'key': file_object.key,
+        'objects': {
+            'sub': {
+                'name': '',
+                'file_type': FileType.DIRECTORY.value,
+                'key': None,
+                'objects': {},
+            }
+        }
+    }
+
+    assert file_object.serialize() == expected
+
+
+def test_serialize_roundtrip(file_object: File):
+    """Test the serialization round trip."""
+    serialized = file_object.serialize()
+    reconstructed = File.from_serialized(serialized)
+
+    assert isinstance(reconstructed, File)
+    assert file_object == reconstructed
+
+
+def test_eq():
+    """Test the ``File.__eq__`` method."""
+    file_object = File()
+
+    # Identity operation
+    assert file_object == file_object  # pylint: disable=comparison-with-itself
+
+    # Identical default copy
+    assert file_object == File()
+
+    # Identical copy with different arguments
+    assert File(name='custom', file_type=FileType.FILE) == File(name='custom', file_type=FileType.FILE)
+
+    # Identical copies with nested objects
+    assert File(objects={'sub': File()}) == File(objects={'sub': File()})
+
+    assert file_object != File(name='custom')
+    assert file_object != File(file_type=FileType.FILE)
+    assert file_object != File(key='123456')
+    assert file_object != File(objects={'sub': File()})