flavianojs/aiida-core

Improve the storage effiency of the `repository_metadata` format
Since now all nodes will have a full representation of the virtual file
hierarchy of their repository content, which is stored in the database
in the `repository_metadata` column, it is crucial that the format of
that metadata is optimized to use as little data as possible, to prevent
the database from bloating unnecessarily.

Currently, we only define two file types:

 * Directories: which can contain an arbitrary number of sub objects
 * Files: which cannot contain objects, but instead have a unique hashkey

This means that we can serialize the metadata, without encoding the
explicit type. If it contains a key, it is necessarily a file. If it is
not a file, it has to be a directory.

The serialization format therefore can make do with simple nested
dictionaries, where each entry either contains the key `k`, indicating
it is a file, or `o`, which can be a dictionary itself, containing the
objects contained within that directory. Note that we can even safely
omit the `o` key for an empty dictionary. Since there is no `k` we know
it has to be a directory anyway.

This logic is implemented in `FileObject.from_serialized` that can fully
reconstruct all instance properties by inferring them from the sparse
serialized data.

aiida/repository/common.py
@@ -35,34 +35,49 @@ class FileObject():
         if objects is not None and any([not isinstance(obj, self.__class__) for obj in objects.values()]):
             raise TypeError('objects should be `None` or a dictionary of `FileObject` instances.')
 
+        if file_type == FileType.DIRECTORY and key is not None:
+            raise ValueError('an object of type `FileType.DIRECTORY` cannot define a key.')
+
+        if file_type == FileType.FILE and objects is not None:
+            raise ValueError('an object of type `FileType.FILE` cannot define any objects.')
+
         self._name = name
         self._file_type = file_type
         self._key = key
         self._objects = objects or {}
 
     @classmethod
-    def from_serialized(cls, serialized: dict) -> 'FileObject':
+    def from_serialized(cls, serialized: dict, name='') -> 'FileObject':
         """Construct a new instance from a serialized instance.
 
         :param serialized: the serialized instance.
         :return: the reconstructed file object.
         """
-        objects = {name: FileObject.from_serialized(obj) for name, obj in serialized['objects'].items()}
+        if 'k' in serialized:
+            file_type = FileType.FILE
+            key = serialized['k']
+            objects = None
+        else:
+            file_type = FileType.DIRECTORY
+            key = None
+            objects = {name: FileObject.from_serialized(obj, name) for name, obj in serialized.get('o', {}).items()}
+
         instance = cls.__new__(cls)
-        instance.__init__(serialized['name'], FileType(serialized['file_type']), serialized['key'], objects)
+        instance.__init__(name, file_type, key, objects)
         return instance
 
     def serialize(self) -> dict:
         """Serialize the metadata into a JSON-serializable format.
 
+        .. note:: the serialization format is optimized to reduce the size in bytes.
+
         :return: dictionary with the content metadata.
         """
-        return {
-            'name': self.name,
-            'file_type': self.file_type.value,
-            'key': self.key,
-            'objects': {key: obj.serialize() for key, obj in self.objects.items()},
-        }
+        if self.file_type == FileType.DIRECTORY:
+            if self.objects:
+                return {'o': {key: obj.serialize() for key, obj in self.objects.items()}}
+            return {}
+        return {'k': self.key}
 
     @property
     def name(self) -> str:
@@ -90,11 +105,11 @@ class FileObject():
             return False
 
         equal_attributes = all([getattr(self, key) == getattr(other, key) for key in ['name', 'file_type', 'key']])
-        equal_object_keys = list(self.objects) == list(other.objects)
+        equal_object_keys = sorted(self.objects) == sorted(other.objects)
         equal_objects = equal_object_keys and all([obj == other.objects[key] for key, obj in self.objects.items()])
 
         return equal_attributes and equal_objects
 
     def __repr__(self):
-        args = (self.name, self.file_type.value, self.key, list(self.objects.keys()))
+        args = (self.name, self.file_type.value, self.key, self.objects.items())
         return 'FileObject<name={}, file_type={}, key={}, objects={}>'.format(*args)

aiida/repository/repository.py
@@ -56,8 +56,8 @@ class Repository:
         instance.__init__(backend)
 
         if serialized:
-            for name, obj in serialized['objects'].items():
-                instance.get_directory().objects[name] = FileObject.from_serialized(obj)
+            for name, obj in serialized['o'].items():
+                instance.get_directory().objects[name] = FileObject.from_serialized(obj, name)
 
         return instance
 

tests/repository/test_common.py
@@ -10,9 +10,9 @@ from aiida.repository import FileObject, FileType
 def file_object() -> FileObject:
     """Test fixture to create and return a ``FileObject`` instance."""
     name = 'relative'
-    file_type = FileType.FILE
-    key = 'abcdef'
-    objects = {'sub': FileObject()}
+    file_type = FileType.DIRECTORY
+    key = None
+    objects = {'sub': FileObject('sub', file_type=FileType.FILE, key='abcdef')}
     return FileObject(name, file_type, key, objects)
 
 
@@ -28,15 +28,26 @@ def test_constructor():
 def test_constructor_kwargs(file_object: FileObject):
     """Test the constructor specifying specific keyword arguments."""
     name = 'relative'
-    file_type = FileType.FILE
-    key = 'abcdef'
+    file_type = FileType.DIRECTORY
+    key = None
     objects = {'sub': FileObject()}
     file_object = FileObject(name, file_type, key, objects)
 
-    assert file_object.name == 'relative'
-    assert file_object.file_type == FileType.FILE
-    assert file_object.key == 'abcdef'
+    assert file_object.name == name
+    assert file_object.file_type == file_type
+    assert file_object.key == key
     assert file_object.objects == objects
+
+    name = 'relative'
+    file_type = FileType.FILE
+    key = 'abcdef'
+    objects = None
+    file_object = FileObject(name, file_type, key, objects)
+
+    assert file_object.name == name
+    assert file_object.file_type == file_type
+    assert file_object.key == key
+    assert file_object.objects == {}
 
 
 def test_constructor_kwargs_invalid():
@@ -55,22 +66,26 @@ def test_constructor_kwargs_invalid():
     with pytest.raises(TypeError):
         FileObject(name, file_type, 123, objects)
 
-    with pytest.raises(TypeError):
-        FileObject(name, file_type, key, {'sub': FileObject, 'wrong': 'type'})
+    with pytest.raises(ValueError, match=r'an object of type `FileType.FILE` cannot define any objects.'):
+        FileObject(name, FileType.FILE, key, {})
+
+    with pytest.raises(ValueError, match=r'an object of type `FileType.DIRECTORY` cannot define a key.'):
+        FileObject(name, FileType.DIRECTORY, key, {})
 
 
-def test_serialize(file_object: FileObject):
+def test_serialize():
     """Test the ``FileObject.serialize`` method."""
+    objects = {
+        'empty': FileObject('empty', file_type=FileType.DIRECTORY),
+        'file.txt': FileObject('file.txt', file_type=FileType.FILE, key='abcdef'),
+    }
+    file_object = FileObject(file_type=FileType.DIRECTORY, objects=objects)
+
     expected = {
-        'name': file_object.name,
-        'file_type': file_object.file_type.value,
-        'key': file_object.key,
-        'objects': {
-            'sub': {
-                'name': '',
-                'file_type': FileType.DIRECTORY.value,
-                'key': None,
-                'objects': {},
+        'o': {
+            'empty': {},
+            'file.txt': {
+                'k': 'abcdef',
             }
         }
     }
@@ -81,7 +96,7 @@ def test_serialize(file_object: FileObject):
 def test_serialize_roundtrip(file_object: FileObject):
     """Test the serialization round trip."""
     serialized = file_object.serialize()
-    reconstructed = FileObject.from_serialized(serialized)
+    reconstructed = FileObject.from_serialized(serialized, file_object.name)
 
     assert isinstance(reconstructed, FileObject)
     assert file_object == reconstructed
@@ -105,5 +120,19 @@ def test_eq():
 
     assert file_object != FileObject(name='custom')
     assert file_object != FileObject(file_type=FileType.FILE)
-    assert file_object != FileObject(key='123456')
+    assert file_object != FileObject(key='123456', file_type=FileType.FILE)
     assert file_object != FileObject(objects={'sub': FileObject()})
+
+    # Test ordering of nested files:
+    objects = {
+        'empty': FileObject('empty', file_type=FileType.DIRECTORY),
+        'file.txt': FileObject('file.txt', file_type=FileType.FILE, key='abcdef'),
+    }
+    file_object_a = FileObject(file_type=FileType.DIRECTORY, objects=objects)
+    objects = {
+        'file.txt': FileObject('file.txt', file_type=FileType.FILE, key='abcdef'),
+        'empty': FileObject('empty', file_type=FileType.DIRECTORY),
+    }
+    file_object_b = FileObject(file_type=FileType.DIRECTORY, objects=objects)
+
+    assert file_object_a == file_object_b

chrisjsewell/aiida_core

aiida/repository/common.py
@@ -43,34 +43,49 @@ class File():
         if objects is not None and any([not isinstance(obj, self.__class__) for obj in objects.values()]):
             raise TypeError('objects should be `None` or a dictionary of `File` instances.')
 
+        if file_type == FileType.DIRECTORY and key is not None:
+            raise ValueError('an object of type `FileType.DIRECTORY` cannot define a key.')
+
+        if file_type == FileType.FILE and objects is not None:
+            raise ValueError('an object of type `FileType.FILE` cannot define any objects.')
+
         self._name = name
         self._file_type = file_type
         self._key = key
         self._objects = objects or {}
 
     @classmethod
-    def from_serialized(cls, serialized: dict) -> 'File':
+    def from_serialized(cls, serialized: dict, name='') -> 'File':
         """Construct a new instance from a serialized instance.
 
         :param serialized: the serialized instance.
         :return: the reconstructed file object.
         """
-        objects = {name: File.from_serialized(obj) for name, obj in serialized['objects'].items()}
+        if 'k' in serialized:
+            file_type = FileType.FILE
+            key = serialized['k']
+            objects = None
+        else:
+            file_type = FileType.DIRECTORY
+            key = None
+            objects = {name: File.from_serialized(obj, name) for name, obj in serialized.get('o', {}).items()}
+
         instance = cls.__new__(cls)
-        instance.__init__(serialized['name'], FileType(serialized['file_type']), serialized['key'], objects)
+        instance.__init__(name, file_type, key, objects)
         return instance
 
     def serialize(self) -> dict:
         """Serialize the metadata into a JSON-serializable format.
 
+        .. note:: the serialization format is optimized to reduce the size in bytes.
+
         :return: dictionary with the content metadata.
         """
-        return {
-            'name': self.name,
-            'file_type': self.file_type.value,
-            'key': self.key,
-            'objects': {key: obj.serialize() for key, obj in self.objects.items()},
-        }
+        if self.file_type == FileType.DIRECTORY:
+            if self.objects:
+                return {'o': {key: obj.serialize() for key, obj in self.objects.items()}}
+            return {}
+        return {'k': self.key}
 
     @property
     def name(self) -> str:
@@ -98,11 +113,11 @@ class File():
             return False
 
         equal_attributes = all([getattr(self, key) == getattr(other, key) for key in ['name', 'file_type', 'key']])
-        equal_object_keys = list(self.objects) == list(other.objects)
+        equal_object_keys = sorted(self.objects) == sorted(other.objects)
         equal_objects = equal_object_keys and all([obj == other.objects[key] for key, obj in self.objects.items()])
 
         return equal_attributes and equal_objects
 
     def __repr__(self):
-        args = (self.name, self.file_type.value, self.key, list(self.objects.keys()))
+        args = (self.name, self.file_type.value, self.key, self.objects.items())
         return 'File<name={}, file_type={}, key={}, objects={}>'.format(*args)

aiida/repository/repository.py
@@ -56,8 +56,8 @@ class Repository:
         instance.__init__(backend)
 
         if serialized:
-            for name, obj in serialized['objects'].items():
-                instance.get_directory().objects[name] = File.from_serialized(obj)
+            for name, obj in serialized['o'].items():
+                instance.get_directory().objects[name] = File.from_serialized(obj, name)
 
         return instance
 

tests/repository/test_common.py
@@ -10,9 +10,9 @@ from aiida.repository import File, FileType
 def file_object() -> File:
     """Test fixture to create and return a ``File`` instance."""
     name = 'relative'
-    file_type = FileType.FILE
-    key = 'abcdef'
-    objects = {'sub': File()}
+    file_type = FileType.DIRECTORY
+    key = None
+    objects = {'sub': File('sub', file_type=FileType.FILE, key='abcdef')}
     return File(name, file_type, key, objects)
 
 
@@ -28,15 +28,26 @@ def test_constructor():
 def test_constructor_kwargs(file_object: File):
     """Test the constructor specifying specific keyword arguments."""
     name = 'relative'
-    file_type = FileType.FILE
-    key = 'abcdef'
+    file_type = FileType.DIRECTORY
+    key = None
     objects = {'sub': File()}
     file_object = File(name, file_type, key, objects)
 
-    assert file_object.name == 'relative'
-    assert file_object.file_type == FileType.FILE
-    assert file_object.key == 'abcdef'
+    assert file_object.name == name
+    assert file_object.file_type == file_type
+    assert file_object.key == key
     assert file_object.objects == objects
+
+    name = 'relative'
+    file_type = FileType.FILE
+    key = 'abcdef'
+    objects = None
+    file_object = File(name, file_type, key, objects)
+
+    assert file_object.name == name
+    assert file_object.file_type == file_type
+    assert file_object.key == key
+    assert file_object.objects == {}
 
 
 def test_constructor_kwargs_invalid():
@@ -55,22 +66,26 @@ def test_constructor_kwargs_invalid():
     with pytest.raises(TypeError):
         File(name, file_type, 123, objects)
 
-    with pytest.raises(TypeError):
-        File(name, file_type, key, {'sub': File, 'wrong': 'type'})
+    with pytest.raises(ValueError, match=r'an object of type `FileType.FILE` cannot define any objects.'):
+        File(name, FileType.FILE, key, {})
+
+    with pytest.raises(ValueError, match=r'an object of type `FileType.DIRECTORY` cannot define a key.'):
+        File(name, FileType.DIRECTORY, key, {})
 
 
-def test_serialize(file_object: File):
+def test_serialize():
     """Test the ``File.serialize`` method."""
+    objects = {
+        'empty': File('empty', file_type=FileType.DIRECTORY),
+        'file.txt': File('file.txt', file_type=FileType.FILE, key='abcdef'),
+    }
+    file_object = File(file_type=FileType.DIRECTORY, objects=objects)
+
     expected = {
-        'name': file_object.name,
-        'file_type': file_object.file_type.value,
-        'key': file_object.key,
-        'objects': {
-            'sub': {
-                'name': '',
-                'file_type': FileType.DIRECTORY.value,
-                'key': None,
-                'objects': {},
+        'o': {
+            'empty': {},
+            'file.txt': {
+                'k': 'abcdef',
             }
         }
     }
@@ -81,7 +96,7 @@ def test_serialize(file_object: File):
 def test_serialize_roundtrip(file_object: File):
     """Test the serialization round trip."""
     serialized = file_object.serialize()
-    reconstructed = File.from_serialized(serialized)
+    reconstructed = File.from_serialized(serialized, file_object.name)
 
     assert isinstance(reconstructed, File)
     assert file_object == reconstructed
@@ -105,5 +120,19 @@ def test_eq():
 
     assert file_object != File(name='custom')
     assert file_object != File(file_type=FileType.FILE)
-    assert file_object != File(key='123456')
+    assert file_object != File(key='123456', file_type=FileType.FILE)
     assert file_object != File(objects={'sub': File()})
+
+    # Test ordering of nested files:
+    objects = {
+        'empty': File('empty', file_type=FileType.DIRECTORY),
+        'file.txt': File('file.txt', file_type=FileType.FILE, key='abcdef'),
+    }
+    file_object_a = File(file_type=FileType.DIRECTORY, objects=objects)
+    objects = {
+        'file.txt': File('file.txt', file_type=FileType.FILE, key='abcdef'),
+        'empty': File('empty', file_type=FileType.DIRECTORY),
+    }
+    file_object_b = File(file_type=FileType.DIRECTORY, objects=objects)
+
+    assert file_object_a == file_object_b